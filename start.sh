b923789f6d1994bb878b377cd6dbf62ebc73ae2b
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-04-13 11:14:13 +0200

    updating start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -r processor0 processor1 processor2 processor3
+rm -r 0.0001
 cartesianMesh
 checkMesh
 echo "Awaiting Mesh OK Directive!"
@@ -8,4 +9,6 @@ read -p "Press [Enter] to start renumbering Mesh"
 renumberMesh
 cp "0/alpha.water.orig" "0/alpha.water"
 setFields
+echo "Were Fields setted?"
+read -p "Press [Enter] to start decomposing!"
 decomposePar
